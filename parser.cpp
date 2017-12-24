a3302cde60eb3242efef23eb2b384fe68b7b0e76
ytliang97 <ytliang97@gmail.com>
2017-12-24 10:54:02 +0800

    Update parser.cpp(skeleton code)

--- parser.cpp
+++ parser.cpp
@@ -1,19 +1,54 @@
-#include <iostream>
+#include <iostream>                                                                                              
 #include <fstream>
 
 int main(int argc, char* argv[]) {
     std::ifstream myfile;
-    char* filename = argv[1]; 
-    myfile.open(filename);
+    char* CFGfile = argv[1];
+    myfile.open(CFGfile);
     if(myfile.is_open()) {
-        std::string line;
-        while(getline(myfile, line)) {
-            std::cout << line << std::endl;                                                                      
-        }   
+        /*parse CFG*/
+
+
+
+
+
+
+        /*end of parse CFG*/
+        int parseOK = 1;
+        if(parseOK) {
+            std::cout << "CFG is ready" << std::endl;
+            /*print parsing table*/
+            std::cout << "parsing table" << std::endl;
+
+
+
+
+
+            /*end of print parsing table*/
+            std::ifstream file;
+            char* testFile = argv[2];
+            file.open(testFile);
+            if(file.is_open()) {
+                std::string tokenString;
+                while(getline(file, tokenString)) {
+                    /*run every tokenString in testFile*/
+
+
+
+
+                    /*end of run every tokenString in testFile*/
+                    std::cout << tokenString + "apply rule" << std::endl;
+                }
+                                            }else {
+                std::cout << "unable to open testFile" << std::endl;
+            }
+        }else {
+            std::cout << "parsing error" << std::endl;
+        }
         myfile.close();
     }else {
-        std::cout << "unable to open file" << std::endl;
-    }   
+        std::cout << "unable to open CFG file" << std::endl;
+    }
 
     return 0;
 }
